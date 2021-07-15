171ae3a9b655c4cb5780d1469796857df010b868
Seamus Lee <seamus.lee@jmaconsulting.biz>
2021-07-15 04:09:15 -0400

    Updates following testing on b2 aegir hostmatser

---
+++ drush/Provision/Service/canadahelps.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The Canada Helps service class.
+ */
+class Provision_Service_canadahelps extends Provision_Service {
+  public $service = 'canadahelps';
+  
+  /**
+   * Add the property to the site context.
+   */
+  static function subscribe_site($context) {
+    $context->setProperty('hosting_canadahelps_d9_id');
+  }
+
+}
